1f303f0bda32f1f7ab51e609563ec2994615f73c
ArungAlam <arung@student.uns.ac.id>
2021-09-14 17:05:38 +0700

    asdad

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
